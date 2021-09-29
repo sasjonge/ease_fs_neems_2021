6234649c54f343edb1eefdc1ec8b5218c25cffe4
Sascha Jongebloed <sasjonge@uni-bremen.de>
2021-09-29 11:04:57 +0200

    some small fix to the task description

--- prolog/exercise.pl
+++ prolog/exercise.pl
@@ -1,5 +1,5 @@
 :- module(exercise,
-	[ robot_parts/2,
+	[ robot_part/2,
 	  robot_part_list/2
 	]).
 
@@ -7,12 +7,13 @@
 % is reachable using the 'http://knowrob.org/kb/srdl2-comp.owl#hasBodyPart' rule. 
 % 
 % You can use the following predicate to get the direct body parts: 
-% triple(Part, 'http://knowrob.org/kb/srdl2-comp.owl#hasBodyPart', Subparts).
+% triple(Part, dul:'hasPart', Subpart).
+% If you want you can define your own helper predicates.
 % You are NOT allowed to use existing transitive predicates in KnowRob.
 robot_part(Part1, Part2) :-
 	Part2 = 'http://knowrob.org/kb/PR2.owl#PR2Gripper_0_L'.
 
-% robot_parts_list should return all body parts that are reachable by a given
+% robot_part_list should return all body parts that are reachable by a given
 % InputPart (see the robot_parts predicate), as a list.
 robot_part_list(InputPart, Partlist) :-
 	Partlist = ['http://knowrob.org/kb/PR2.owl#PR2Gripper_0_L'].
